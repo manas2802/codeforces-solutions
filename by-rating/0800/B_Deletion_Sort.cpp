ebbe90cbbf5cfd949bb8f52616e66b90e0c0a30a
manas2802 <manasmangalam028@gmail.com>
2026-03-01 01:32:08 +0530

    Add solution for B. Deletion Sort problem
    Implemented B. Deletion Sort problem solution.

--- by-rating/0800/B_Deletion_Sort.cpp
+++ by-rating/0800/B_Deletion_Sort.cpp
@@ -1,1 +1,27 @@
+  // Problem: B. Deletion Sort
+  // Time: O(n) per test case
+  // Space: O(n)
+
+  #include <bits/stdc++.h>
+  using namespace std;
+
+  void solve() {
+      int n;
+      cin >> n;
+      vector<int> a(n);
+      for (auto &i : a) cin >> i;
+
+      if (is_sorted(a.begin(), a.end())) cout << n << '\n';
+      else cout << 1 << '\n';
+  }
+
+  int main() {
+      ios::sync_with_stdio(false);
+      cin.tie(nullptr);
+
+      int t;
+      cin >> t;
+      while (t--) solve();
+      return 0;
+  }
 
